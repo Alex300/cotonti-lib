e7f6ba747379ddbbd1a43e8f0127467285fbd3ee
Alexey Kalnov <kalnovalexey@yandex.ru>
2015-06-25 12:44:38 +0300

    fix php doc in View.php

--- lib/View.php
+++ lib/View.php
@@ -278,15 +278,15 @@ class View{
     }
 
     /**
-     * @param $viewFile имя файла шаблона. Если не указано расширение или оно не входит в $_extensions, будет использовано
-     *              '.php'
+     * @param array|string $viewFile имя файла шаблона. Если не указано расширение или оно не входит в $_extensions,
+     *              будет использовано '.php'
      *              The default search order is:
      *                1) Current theme folder (plugins/ subdir for plugins, admin/ subdir for admin)
      *                2) Default theme folder (if current is not default)
      *                3) tpl subdir in module/plugin folder (fallback template)
      * @param string $type Extension type: 'plug', 'module' or 'core'
-     * @param null $admin  Use admin theme file if present. Tries to determine from viewFile string by default.
-     * @param bool $return Вернуть как строку?
+     * @param null   $admin  Use admin theme file if present. Tries to determine from viewFile string by default.
+     * @param bool   $return Вернуть как строку?
      * @return string
      * @throws Exception
      */
